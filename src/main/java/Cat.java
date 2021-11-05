e25eb2a1412b1855ed62a4173921ee3f5a90469d
vladi <vladimir.matveyev@outlook.com>
2021-11-06 00:50:07 +0300

    complete task_212

---
+++ src/main/java/Cat.java
@@ -0,0 +1,23 @@
+public class Cat {
+    public String name;
+    private String message;
+
+    public Cat() {
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
